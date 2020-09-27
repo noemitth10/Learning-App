dd0a4e6450b822135d4614c8ed656326c860c212
noemitth10 <noemitth10@users.noreply.github.com>
2020-09-27 21:51:44 +0200

    Paired test answers checking

--- frontend/src/Components/Demo_test/Paired_Test.js
+++ frontend/src/Components/Demo_test/Paired_Test.js
@@ -12,6 +12,28 @@ const Paired_Test = ({
   answers,
   points,
 }) => {
+  var pointed_first_id = 0;
+  var pointed_second_id = 0;
+  var chosen_points = 0;
+
+  const setId = (id) => {
+    if (chosen_points == 0) {
+      chosen_points += 1;
+      pointed_first_id = id;
+      console.log("First id: " + pointed_first_id);
+    } else {
+      chosen_points += 1;
+      pointed_second_id = id;
+      console.log("Second id: " + pointed_second_id);
+    }
+
+    if (chosen_points == 2) {
+      if (pointed_first_id == pointed_second_id) console.log("good answer");
+      else console.log("bad answer");
+      chosen_points = 0;
+    }
+  };
+
   return (
     <div>
       <div className="title-container">{category}</div>
@@ -19,12 +41,22 @@ const Paired_Test = ({
       <div className="question-container">
         <div className="left-side">
           {answers.map((answer) => (
-            <Button className="outline-button">{answer.question_text}</Button>
+            <Button
+              className="outline-button"
+              onClick={() => setId(answer.answer_id)}
+            >
+              {answer.question_text}
+            </Button>
           ))}
         </div>
         <div className="right-side">
           {answers.map((answer) => (
-            <Button className="outline-button">{answer.answer_text}</Button>
+            <Button
+              className="outline-button"
+              onClick={() => setId(answer.answer_id)}
+            >
+              {answer.answer_text}
+            </Button>
           ))}
         </div>
       </div>
